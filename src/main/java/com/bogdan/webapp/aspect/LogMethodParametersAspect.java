4741bac2e96a07d9a552e50ec02aadfdc9b33cc8
Bogdan Stefoni <bogdanstefoni.wd@gmail.com>
2021-09-17 11:52:57 +0300

    Tested the Parameter Names and Parameter Values by assigning to new variables and looping through.

--- src/main/java/com/bogdan/webapp/aspect/LogMethodParametersAspect.java
+++ src/main/java/com/bogdan/webapp/aspect/LogMethodParametersAspect.java
@@ -3,7 +3,7 @@ package com.bogdan.webapp.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.reflect.CodeSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -19,13 +19,20 @@ public class LogMethodParametersAspect {
     @Around("@annotation(com.bogdan.webapp.annotation.LogMethodParameters)")
     public Object logMethodParameters(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
-        MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
+        CodeSignature signature = (CodeSignature) proceedingJoinPoint.getSignature();
 
         Arrays.stream(signature.getParameterNames())
-                .forEach(o -> logger.info("Parameter values: " + o));
+                .forEach(o -> logger.info("Parameter names: " + o));
 
         Arrays.stream(proceedingJoinPoint.getArgs())
-                .forEach(s -> logger.info("Args values: " + s.toString()));
+                .forEach(s -> logger.info("Parameter values: " + s.toString()));
+
+        String[] parameterNames = signature.getParameterNames();
+        Object[] parameterValues = proceedingJoinPoint.getArgs();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            logger.info("Parameter name: " + parameterNames[i] + ". Parameter value: " + parameterValues[i]);
+        }
 
 
         return proceedingJoinPoint.proceed();
