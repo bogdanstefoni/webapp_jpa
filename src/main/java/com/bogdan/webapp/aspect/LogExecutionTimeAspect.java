ae2e7776aa6aea8a5d22a42601033c918652fb6d
andrei_daniel_manolache <andrei.manolache@ro.ibm.com>
2021-10-07 15:01:52 +0300

    Cosmetics

--- src/main/java/com/bogdan/webapp/aspect/LogExecutionTimeAspect.java
+++ src/main/java/com/bogdan/webapp/aspect/LogExecutionTimeAspect.java
@@ -1,5 +1,8 @@
 package com.bogdan.webapp.aspect;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,34 +10,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-
 @Aspect
 @Component
 public class LogExecutionTimeAspect {
 
-    private final Logger logger = LoggerFactory.getLogger(LogExecutionTimeAspect.class);
+	private final Logger logger = LoggerFactory.getLogger(LogExecutionTimeAspect.class);
 
-    private static String getDurationInSeconds(long duration) {
-        NumberFormat numberFormat = new DecimalFormat("#0.00");
-        return numberFormat.format(duration / 1000d);
-    }
+	private static String getDurationInSeconds(long duration) {
+		NumberFormat numberFormat = new DecimalFormat("#0.00");
+		return numberFormat.format(duration / 1000d);
+	}
 
-    @Around("@annotation(com.bogdan.webapp.annotation.LogExecutionTime)")
-    public Object logExecutionTime(
-            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+	@Around("@annotation(com.bogdan.webapp.annotation.LogExecutionTime)")
+	public Object logExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
-        long start = System.currentTimeMillis();
-        Object proceed = proceedingJoinPoint.proceed();
-        String executionTime = getDurationInSeconds(System.currentTimeMillis() - start);
+		long start = System.currentTimeMillis();
+		Object proceed = proceedingJoinPoint.proceed();
+		String executionTime = getDurationInSeconds(System.currentTimeMillis() - start);
 
-        logger.info("Execution time for method: " + proceedingJoinPoint.getSignature()
-                .getDeclaringTypeName() + "." + proceedingJoinPoint.getSignature().getName() + ": "
-                + executionTime);
+		logger.info("Execution time for method <" + proceedingJoinPoint.getSignature().getDeclaringTypeName() + "."
+				+ proceedingJoinPoint.getSignature().getName() + ">: " + executionTime);
 
-        return proceed;
+		return proceed;
 
-    }
+	}
 
 }
--- src/main/java/com/bogdan/webapp/aspect/LogMethodParametersAspect.java
+++ src/main/java/com/bogdan/webapp/aspect/LogMethodParametersAspect.java
@@ -8,35 +8,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Aspect
 @Component
 public class LogMethodParametersAspect {
 
-    private final Logger logger = LoggerFactory.getLogger(LogMethodParametersAspect.class);
+	private final Logger logger = LoggerFactory.getLogger(LogMethodParametersAspect.class);
 
-    @Around("@annotation(com.bogdan.webapp.annotation.LogMethodParameters)")
-    public Object logMethodParameters(
-            ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+	@Around("@annotation(com.bogdan.webapp.annotation.LogMethodParameters)")
+	public Object logMethodParameters(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
-        CodeSignature signature = (CodeSignature) proceedingJoinPoint.getSignature();
+		CodeSignature signature = (CodeSignature) proceedingJoinPoint.getSignature();
 
-        Arrays.stream(signature.getParameterNames())
-                .forEach(o -> logger.info("Parameter names: " + o));
+		String[] parameterNames = signature.getParameterNames();
+		Object[] parameterValues = proceedingJoinPoint.getArgs();
 
-        Arrays.stream(proceedingJoinPoint.getArgs())
-                .forEach(s -> logger.info("Parameter values: " + s.toString()));
+		for (int i = 0; i < parameterNames.length; i++) {
+			logger.info("Parameter name: " + parameterNames[i] + ". Parameter value: " + parameterValues[i]);
+		}
 
-        String[] parameterNames = signature.getParameterNames();
-        Object[] parameterValues = proceedingJoinPoint.getArgs();
-
-        for (int i = 0; i < parameterNames.length; i++) {
-            logger.info("Parameter name: " + parameterNames[i] + ". Parameter value: "
-                    + parameterValues[i]);
-        }
-
-        return proceedingJoinPoint.proceed();
-    }
+		return proceedingJoinPoint.proceed();
+	}
 
 }
